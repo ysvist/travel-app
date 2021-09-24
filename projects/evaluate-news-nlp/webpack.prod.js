555f055042d06d909ecbcfb13b4b421bb387dc41
Yuli <yuliya@kiwico.com>
2021-09-24 16:29:20 -0700

    temp: Commit broken service worker for help :(

--- projects/evaluate-news-nlp/webpack.prod.js
+++ projects/evaluate-news-nlp/webpack.prod.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const webpack = require("webpack");
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const WorkboxPlugin = require("workbox-webpack-plugin");
 
 module.exports = {
   entry: "./src/client/index.js",
@@ -23,6 +24,7 @@ module.exports = {
       template: "./src/client/views/index.html",
       filename: "./index.html",
     }),
+    new WorkboxPlugin.GenerateSW(),
   ],
   output: {
     libraryTarget: "var",
